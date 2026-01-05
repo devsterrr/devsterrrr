4a6159239cfc7789e85ce8bb4bc367db59223f0b
unknown <herasimenkoYaroslav@gmail.com>
2026-01-05 20:28:48 +0200

    first commit

---
+++ les9.py
@@ -0,0 +1,20 @@
+# import random 
+# print(random.choice(['apple', 'banana','cherry']))                   # ------ЦУЄФА-----
+# print(random.randint(1,10))
+# print(random.random())
+
+
+
+# from random import randint                              ===Частина модуля===
+# print(randint(1,10))
+
+# import random as r
+# print(r.choice(['apple', 'banana', 'cherry']))
+# print(r.randint(1,10))
+
+import time
+# print(time.time())
+# print(time.ctime())
+# # print(time.localtime())
+# time.sleep(2)
+# print("2 sec later")
